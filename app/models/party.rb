fd5e0bc16780359863544c139d8eafe51d4ceb22
bastienreb <bastien.rebotton@gmail.com>
2019-08-13 12:00:52 +0200

    Jour 2

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,34 +1,45 @@
 class Party < ApplicationRecord
-	validates :title, presence: true, length: {in: 5..140}
-	validates :description, presence: true, length: {in: 20..1000}
-	validates :price, presence: true, inclusion: 1..1000
-	validates :location, presence: true
+  after_create :new_participation
 
+  def new_participation
+    EventMailer.new_participation(self).deliver_now
+  end
 
+  validates :start_date,
+    presence: true
+    validate :in_the_past?
+  validates :duration,
+    presence: true,
+    numericality: { greater_than_or_equal_to: 5 }
+    validate :multiple_of_5?
+  validates :title,
+    presence: true,
+    length: { in: 5..140 }
+  validates :description,
+    presence: true,
+    length: { in: 20..1000 }
+  validates :price,
+    presence: true,
+    length: { in: 1..1000 }
+  validates :location,
+    presence: true
 
+  has_many :participations
+  has_many :users, through: :participations
+  belongs_to :user
 
+  private
 
-	validates :start_date, presence: true, validates_with: is_in_the_past?
-    private
-      def is_in_the_past?(start_date)
-        if start_date < Time.now
-          	errors.add("The start date cannot be in the past!")
-        	end
-    	end
-    end
+  def multiple_of_5?
+    return duration unless duration % 5 != 0 || duration.negative?
+      errors.add(
+        :duration,
+        'The duration must end by 0 ou 5 and be a positive number.'
+      )
+  end
 
-    validates :duration, presence: true, numericality: { greater_than_or_equal_to: 5 }, validates_with: is_multiple_of_5?
-    private
-      def is_multiple_of_5?(duration)
-      if duration%5 != 0
-        errors.add("The duration must be a multiple of 5!")
-      	end
-	end
-
-
-
-
-	has_many :participations
-	has_many :users through: :participations
+  def in_the_past?
+    return start_date unless start_date < Time.now
+      errors.add(:start_date, 'The start date cannot be in the past!')
+  end
 end
-
